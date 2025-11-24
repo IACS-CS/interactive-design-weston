01d1da6064f7bd0351eecb4e487163e98aabcb80
westonsoun-dam-crypto <weston.soun-dam@innovationcharter.org>
2025-11-24 17:29:21 +0000

    added questions and buttons

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log("Hello, Interactive Graphic Design!");
 // This is the main JavaScript file for the Interactive Graphic Design project.
+document.querySelector("#start-button").addEventListener(
+  // when they click the start button
+  "click",
+  function (event) {
+    // get the parent section of the button
+    const parentSection = event.target.closest("section");
+    // find the next section and make it active
+    parentSection.nextElementSibling.classList.add("active");
+    // hide the current section
+    parentSection.classList.remove("active");
+  }
+);
